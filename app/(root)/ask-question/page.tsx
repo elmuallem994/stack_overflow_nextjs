09c1d52e12486681fb69d7accc4b7807e5241ea3
almoallemlamas <elmuallem994@gmail.com>
2024-08-19 01:30:20 +0300

    'ssss'

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,8 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs/server";
-
 import { redirect } from "next/navigation";
+import React from "react";
 
 const Page = async () => {
   const { userId } = auth();
@@ -16,7 +16,7 @@ const Page = async () => {
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
