1f383ba73433c99c702bbbf31bc08e14cbd1f14f
almoallemlamas <elmuallem994@gmail.com>
2024-08-18 21:51:51 +0300

    'auth'

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,18 +1,16 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs/server";
+
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  //const { userId } = auth();
-
-  const userId = "clerk12345";
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
